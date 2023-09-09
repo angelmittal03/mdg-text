69e2d84927ffcabd5e0cfcf4ec9341d98e502c76
the-geek-deep <deepumrajkar15@gmail.com>
2023-09-09 14:27:06 +0530

    chore : CHanged file and folder structure

--- backend/api/routes/allRoutes.ts
+++ backend/api/routes/allRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import {signup, signin} from "../controllers/users.ts";
-import { socketConnection } from "../controllers/webSocket.ts";
+
  const route = new Router();
 
 route.post("/api/signup", signup)
--- backend/api/schema/user.ts
+++ backend/api/schema/user.ts
@@ -1,8 +1,9 @@
 import {ObjectId} from "https://deno.land/x/mongo@v0.30.0/mod.ts";
 
 export interface UserSchema {
-    _id: ObjectId;
-    username: string;
-    password: string;
+    _id: ObjectId,
+    username: string,
+    password: string,
   }
   
+
